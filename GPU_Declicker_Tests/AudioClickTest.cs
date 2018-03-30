43e216bb86a4cbac84201f9420ac8f3cc8ad4f06
DmitriiKh <31587652+DmitriiKh@users.noreply.github.com>
2018-03-30 10:42:39 -0300

    Reformated AudioClickTests to fit common rules

--- GPU_Declicker_Tests/AudioClickTest.cs
+++ GPU_Declicker_Tests/AudioClickTest.cs
@@ -5,7 +5,7 @@ using System;
 namespace GPU_Declicker_Tests
 {
     [TestClass]
-    public class AudioClickTest
+    public class AudioClickTests
     {
         private AudioClick audioClickFirst;
         private AudioClick audioClickTheSame;
@@ -40,16 +40,7 @@ namespace GPU_Declicker_Tests
         }
 
         [TestMethod]
-        public void AudioClickConstractorTest()
-        {
-            Assert.AreEqual(1111, audioClickFirst.Position);
-            Assert.AreEqual(10, audioClickFirst.Lenght);
-            Assert.AreEqual(3.7F, audioClickFirst.Threshold_level_detected);
-            Assert.AreEqual(ChannelType.Left, audioClickFirst.FromChannel);
-        }
-
-        [TestMethod]
-        public void AudioClickCompareToTest()
+        public void CompareTo_ClickWithBiggerPosition_ReturnsNegative()
         {
             // CompareTo
             // A value that indicates the relative order of the objects being compared. 
@@ -59,128 +50,276 @@ namespace GPU_Declicker_Tests
             // Greater than zero: This instance follows other in the sort order.
             Assert.IsTrue(audioClickFirst.CompareTo(audioClickSecond) < 0,
                 "Failed CompareTo: should return negative");
+        }
+
+        [TestMethod]
+        public void CompareTo_ClickWithSmallerPosition_ReturnsPositive()
+        {
             Assert.IsTrue(audioClickSecond.CompareTo(audioClickFirst) > 0,
                 "Failed CompareTo: should return positive");
+        }
+
+        [TestMethod]
+        public void CompareTo_TheSameClick_ReturnsZero()
+        { 
             Assert.IsTrue(audioClickFirst.CompareTo(audioClickTheSame) == 0,
                 "Failed CompareTo: should be zero (the same)");
+        }
+
+        [TestMethod]
+        public void CompareTo_EqualClick_ReturnsZero()
+        {
             Assert.IsTrue(audioClickFirst.CompareTo(audioClickEqual) == 0,
                 "Failed CompareTo: should be zero (equal)");
         }
 
         [TestMethod]
-        public void AudioClickEqualsTest()
+        public void Equals_TheSameClick_ReturnsTrue()
         {
             Assert.IsTrue(audioClickFirst.Equals(audioClickTheSame),
                 "Failed Equals: should be true (the same)");
-            Assert.IsTrue(audioClickFirst.Equals(audioClickEqual),
-                "Failed Equals: should be true (equal)");
-            Assert.IsFalse(audioClickFirst.Equals(audioClickSecond),
-                "Failed Equals: should be false (the same)");
         }
 
         [TestMethod]
-        public void AudioClickGetHashCodeTest()
+        public void Equals_EqualClick_ReturnsTrue()
         {
-            Assert.AreEqual(audioClickFirst.GetHashCode(), audioClickTheSame.GetHashCode(),
+            Assert.IsTrue(audioClickFirst.Equals(audioClickEqual),
+                "Failed Equals: should be true (equal)");
+        }
+
+        [TestMethod]
+        public void Equals_DifferentClick_ReturnsFalse()
+        {
+            Assert.IsFalse(audioClickFirst.Equals(audioClickSecond),
+                "Failed Equals: should be false (different)");
+        }
+
+        [TestMethod]
+        public void GetHashCode_TheSameClick_ReturnsTheSameHashCode()
+        {
+            Assert.AreEqual(audioClickFirst.GetHashCode(), 
+                audioClickTheSame.GetHashCode(),
                 "Failed GetHashCode: should be equal (the same)");
-            Assert.AreEqual(audioClickFirst.GetHashCode(), audioClickEqual.GetHashCode(),
+        }
+
+        [TestMethod]
+        public void GetHashCode_EqualClicks_ReturnsTheSameHashCode()
+        {
+            Assert.AreEqual(audioClickFirst.GetHashCode(), 
+                audioClickEqual.GetHashCode(),
                 "Failed GetHashCode: should be equal (equal)");
-            Assert.AreNotEqual(audioClickFirst.GetHashCode(), audioClickDifferentPosition.GetHashCode(),
+        }
+
+        [TestMethod]
+        public void GetHashCode_ClicksWithDifferentPositions_ReturnDifferentHashCodes()
+        {
+            Assert.AreNotEqual(audioClickFirst.GetHashCode(), 
+                audioClickDifferentPosition.GetHashCode(),
                 "Failed GetHashCode: should be not equal (different position)");
+        }
+
+        [TestMethod]
+        public void GetHashCode_ClicksWithDifferentLength_ReturnDifferentHaskCodes()
+        {
             Assert.AreNotEqual(audioClickFirst.GetHashCode(), audioClickDifferentLength.GetHashCode(),
                 "Failed GetHashCode: should be not equal (different length)");
+        }
+
+        [TestMethod]
+        public void GetHashCode_ClicksFromDifferentChannels_ReturnDifferentHashCodes()
+        {
             Assert.AreNotEqual(audioClickFirst.GetHashCode(), audioClickDifferentChannel.GetHashCode(),
                 "Failed GetHashCode: should be not equal (different channel)");
         }
 
         [TestMethod]
-        public void AudioClickOperatorEqualTest()
+        public void OperatorEqual_TheSameClick_ReturnsTrue()
         {
             Assert.IsTrue(audioClickFirst == audioClickTheSame,
                 "Failed Operator ==: should be true (the same)");
+        }
+
+        [TestMethod]
+        public void OperatorEqual_ClickWithAllTheSameValues_ReturnsTrue()
+        {
             Assert.IsTrue(audioClickFirst == audioClickEqual,
                 "Failed Operator ==: should be true (equal)");
+        }
+
+        [TestMethod]
+        public void OperatorEqual_ClicksWithDifferentPjsitiions_ReturnsFalse()
+        {
             Assert.IsFalse(audioClickFirst == audioClickDifferentPosition,
                 "Failed Operator ==: should be false (different position)");
+        }
+
+        [TestMethod]
+        public void OperatorEqual_ClicksWithDifferentLengths_ReturnsFalse()
+        {
             Assert.IsFalse(audioClickFirst == audioClickDifferentLength,
                 "Failed Operator ==: should be false (different length)");
+        }
+
+        [TestMethod]
+        public void OperatorEqual_ClicksFromDifferentChannels_ReturnsFalse()
+        {
             Assert.IsFalse(audioClickFirst == audioClickDifferentChannel,
                 "Failed Operator ==: should be false (different channel)");
         }
 
         [TestMethod]
-        public void AudioClickOperatorNotEqualTest()
+        public void OperatorNotEqual_TheSameClick_ReturnsFalse()
         {
             Assert.IsFalse(audioClickFirst != audioClickTheSame,
                 "Failed Operator !=: should be false (the same)");
-            Assert.IsFalse(audioClickFirst != audioClickEqual,
-                "Failed Operator !=: should be false (equal)");
-            Assert.IsTrue(audioClickFirst != audioClickDifferentPosition,
-                "Failed Operator !=: should be true (different position)");
-            Assert.IsTrue(audioClickFirst != audioClickDifferentLength,
-                "Failed Operator !=: should be true (different length)");
-            Assert.IsTrue(audioClickFirst != audioClickDifferentChannel,
-                "Failed Operator !=: should be true (different channel)");
         }
 
         [TestMethod]
-        public void AudioClickOperatorLessTest()
+        public void OperatorNotEqual_ClicksWithAllTheSameValues_ReturnsFalse()
+        {
+            Assert.IsFalse(audioClickFirst != audioClickEqual,
+                "Failed Operator !=: should be false (equal)");
+        }
+
+        [TestMethod]
+        public void OperatorNotEqual_ClicksWithDifferentPositions_ReturnsTrue()
+        {
+            Assert.IsTrue(audioClickFirst != audioClickDifferentPosition,
+                "Failed Operator !=: should be true (different position)");
+        }
+
+        [TestMethod]
+        public void OperatorNotEqual_ClicksWithDifferentLength_ReturnsTrue()
+        {
+            Assert.IsTrue(audioClickFirst != audioClickDifferentLength,
+                "Failed Operator !=: should be true (different length)");
+        }
+
+        [TestMethod]
+        public void OperatorNotEqual_ClicksDifferentChannels_ReturnsTrue()
+        {
+            Assert.IsTrue(audioClickFirst != audioClickDifferentChannel,
+                "Failed Operator !=: should be true (different channel)");
+        }
+        
+        [TestMethod]
+        public void OperatorLess_ComparitionToLesserPositionClick_ReturnsFalse()
         {
             Assert.IsFalse(audioClickFirst < audioClickLesserPosition,
                 "Failed Operator <: should be false (lesser position)");
+        }
+
+        [TestMethod]
+        public void OperatorLess_ComparitionToTheSameClick_ReturnsFalse()
+        {
             Assert.IsFalse(audioClickFirst < audioClickTheSame,
                 "Failed Operator <: should be false (the same)");
+        }
+
+        [TestMethod]
+        public void OperatorLess_ComparitionToEqualClick_ReturnsFalse()
+        {
             Assert.IsFalse(audioClickFirst < audioClickEqual,
                 "Failed Operator <: should be false (equal)");
+        }
+
+        [TestMethod]
+        public void OperatorLess_ComparitionToLargerPositionClick_ReturnsTrue()
+        {
             Assert.IsTrue(audioClickFirst < audioClickLargerPosition,
                 "Failed Operator <: should be true " +
                 "(larger position of second operand)");
         }
 
         [TestMethod]
-        public void AudioClickOperatorLessOrEqualTest()
+        public void OperatorLessOrEqual_ComparitionToLesserPositionClick_ReturnsFals()
         {
             Assert.IsFalse(audioClickFirst <= audioClickLesserPosition,
                 "Failed Operator <=: should be false (lesser position)");
+        }
+
+        [TestMethod]
+        public void OperatorLessOrEqual_ComparitionToTheSameClick_ReturnsTrue()
+        {
             Assert.IsTrue(audioClickFirst <= audioClickTheSame,
                 "Failed Operator <=: should be true (the same)");
+        }
+
+        [TestMethod]
+        public void OperatorLessOrEqual_ComparitionToEqualClick_ReturnsTrue()
+        {
             Assert.IsTrue(audioClickFirst <= audioClickEqual,
                 "Failed Operator <=: should be true (equal)");
+        }
+
+        [TestMethod]
+        public void OperatorLessOrEqual_ComparitionToLargerPositionClick_ReturnsTrue()
+        {
             Assert.IsTrue(audioClickFirst <= audioClickLargerPosition,
                 "Failed Operator <=: should be true " +
                 "(larger position of second operand)");
         }
 
         [TestMethod]
-        public void AudioClickOperatorMoreTest()
+        public void OperatorMore_ComparitionToLesserPositionClick_ReturnsTrue()
         {
             Assert.IsTrue(audioClickFirst > audioClickLesserPosition,
                 "Failed Operator >: should be true (lesser position)");
+        }
+
+        [TestMethod]
+        public void OperatorMore_ComparitionToTheSameClick_ReturnsFalse()
+        {
             Assert.IsFalse(audioClickFirst > audioClickTheSame,
                 "Failed Operator >: should be false (the same)");
+        }
+
+        [TestMethod]
+        public void OperatorMore_ComparitionToEqualClick_ReturnsFalse()
+        {
             Assert.IsFalse(audioClickFirst > audioClickEqual,
                 "Failed Operator >: should be false (equal)");
+        }
+
+        [TestMethod]
+        public void OperatorMore_ComparitionToLargerPositionClick_ReturnsTrue()
+        {
             Assert.IsFalse(audioClickFirst > audioClickLargerPosition,
                 "Failed Operator >: should be true " +
                 "(larger position of second operand)");
         }
 
         [TestMethod]
-        public void AudioClickOperatorMoreOrEqualTest()
+        public void OperatorMoreOrEqual_ComparitionToLesserPositionClick_ReturnsTrue()
         {
             Assert.IsTrue(audioClickFirst >= audioClickLesserPosition,
                 "Failed Operator >=: should be true (lesser position)");
+        }
+
+        [TestMethod]
+        public void OperatorMoreOrEqual_ComparitionToTheSameClick_ReturnsTrue()
+        {
             Assert.IsTrue(audioClickFirst >= audioClickTheSame,
                 "Failed Operator >=: should be true (the same)");
+        }
+
+        [TestMethod]
+        public void OperatorMoreOrEqual_ComparitionToEqualClick_ReturnsTrue()
+        {
             Assert.IsTrue(audioClickFirst >= audioClickEqual,
                 "Failed Operator >=: should be true (equal)");
+        }
+
+        [TestMethod]
+        public void OperatorMoreOrEqual_ComparitionToLargerPositionClick_ReturnsTrue()
+        {
             Assert.IsFalse(audioClickFirst >= audioClickLargerPosition,
                 "Failed Operator >=: should be true " +
                 "(larger position of second operand)");
         }
 
         [TestMethod]
-        public void AudioClickChangeAprovedTest()
+        public void ChangeAproved_SwitchingTwice_SwitchesAproved()
         {
             bool aprovedState = audioClickFirst.Aproved;
             // first change
@@ -193,176 +332,6 @@ namespace GPU_Declicker_Tests
             Assert.AreEqual(audioClickFirst.Aproved, aprovedState,
                 "Failed ChangeAproved (second change): should be "
                 + aprovedState.ToString());
-        }
-
-        [TestMethod]
-        public void AudioClickGetInputSampleTest()
-        {
-            float[] inputAudio = new float[100];
-
-            for (int index = 0; index < inputAudio.Length; index++)
-                inputAudio[index] = (float)Math.Sin(
-                    2 * Math.PI * index / (inputAudio.Length / 5.3));
-            AudioData audioDataForTest = new AudioDataMono(inputAudio);
-
-            AudioClick audioClickForTest = new AudioClick(
-                audioDataForTest.LengthSamples() / 3,
-                audioDataForTest.LengthSamples() / 10,
-                10F,
-                audioDataForTest,
-                ChannelType.Left);
-
-            for (int index = 0; index < audioDataForTest.LengthSamples(); index++)
-                Assert.AreEqual(
-                    audioClickForTest.GetInputSample(index),
-                    audioDataForTest.GetInputSample(index),
-                    "Failed GetInputSample for index" +
-                    index.ToString());
-        }
-
-        [TestMethod]
-        public void AudioClickGetOutputSampleTest()
-        {
-            float[] inputAudio = new float[100];
-
-            for (int index = 0; index < inputAudio.Length; index++)
-                inputAudio[index] = (float)Math.Sin(
-                    2 * Math.PI * index / (inputAudio.Length / 5.3));
-            AudioData audioDataForTest = new AudioDataMono(inputAudio);
-            for (int index = 0; index < inputAudio.Length; index++)
-                audioDataForTest.SetOutputSample(
-                    index,
-                    audioDataForTest.GetInputSample(index));
-
-            AudioClick audioClickForTest = new AudioClick(
-            audioDataForTest.LengthSamples() / 3,
-            audioDataForTest.LengthSamples() / 10,
-            10F,
-            audioDataForTest,
-            ChannelType.Left);
-
-            for (int index = 0; index < audioDataForTest.LengthSamples(); index++)
-                Assert.AreEqual(
-                    audioClickForTest.GetOutputSample(index),
-                    audioDataForTest.GetOutputSample(index),
-                    "Failed GetInputSample for index" +
-                    index.ToString());
-        }
-
-        [TestMethod]
-        public void AudioClickShrinkLeftTest()
-        {
-            float[] inputAudio = new float[4000];
-
-            for (int index = 0; index < inputAudio.Length; index++)
-                inputAudio[index] = (float)Math.Sin(
-                    2 * Math.PI * index / (inputAudio.Length / 5.3));
-            AudioData audioDataForTest = new AudioDataMono(inputAudio);
-            
-            int initialPosition = 2000;
-            int initialLength = 50;
-            AudioClick audioClickForTest = new AudioClick(
-                initialPosition, 
-                initialLength, 
-                10,
-                audioDataForTest, 
-                ChannelType.Left);
-
-            audioClickForTest.ShrinkLeft();
-
-            Assert.AreEqual(audioClickForTest.Position,
-                initialPosition + 1,
-                "Failed ShrinkLeft: position is not right");
-            Assert.AreEqual(audioClickForTest.Lenght,
-                initialLength - 1,
-                "Failed ShrinkLeft: length is not right");
-        }
-
-        [TestMethod]
-        public void AudioClickShrinkRightTest()
-        {
-            float[] inputAudio = new float[4000];
-
-            for (int index = 0; index < inputAudio.Length; index++)
-                inputAudio[index] = (float)Math.Sin(
-                    2 * Math.PI * index / (inputAudio.Length / 5.3));
-            AudioData audioDataForTest = new AudioDataMono(inputAudio);
-
-            int initialPosition = 2000;
-            int initialLength = 50;
-            AudioClick audioClickForTest = new AudioClick(
-                initialPosition,
-                initialLength,
-                10,
-                audioDataForTest,
-                ChannelType.Left);
-
-            audioClickForTest.ShrinkRight();
-
-            Assert.AreEqual(audioClickForTest.Position,
-                initialPosition,
-                "Failed ShrinkRight: position is not right");
-            Assert.AreEqual(audioClickForTest.Lenght,
-                initialLength - 1,
-                "Failed ShrinkRight: length is not right");
-        }
-
-        [TestMethod]
-        public void AudioClickExpandLeftTest()
-        {
-            float[] inputAudio = new float[4000];
-
-            for (int index = 0; index < inputAudio.Length; index++)
-                inputAudio[index] = (float)Math.Sin(
-                    2 * Math.PI * index / (inputAudio.Length / 5.3));
-            AudioData audioDataForTest = new AudioDataMono(inputAudio);
-
-            int initialPosition = 2000;
-            int initialLength = 50;
-            AudioClick audioClickForTest = new AudioClick(
-                initialPosition,
-                initialLength,
-                10,
-                audioDataForTest,
-                ChannelType.Left);
-
-            audioClickForTest.ExpandLeft();
-
-            Assert.AreEqual(audioClickForTest.Position,
-                initialPosition - 1,
-                "Failed ExpandLeft: position is not right");
-            Assert.AreEqual(audioClickForTest.Lenght,
-                initialLength + 1,
-                "Failed ExpandLeft: length is not right");
-        }
-
-        [TestMethod]
-        public void AudioClickExpandRightTest()
-        {
-            float[] inputAudio = new float[4000];
-
-            for (int index = 0; index < inputAudio.Length; index++)
-                inputAudio[index] = (float)Math.Sin(
-                    2 * Math.PI * index / (inputAudio.Length / 5.3));
-            AudioData audioDataForTest = new AudioDataMono(inputAudio);
-
-            int initialPosition = 2000;
-            int initialLength = 50;
-            AudioClick audioClickForTest = new AudioClick(
-                initialPosition,
-                initialLength,
-                10,
-                audioDataForTest,
-                ChannelType.Left);
-
-            audioClickForTest.ExpandRight();
-
-            Assert.AreEqual(audioClickForTest.Position,
-                initialPosition,
-                "Failed ExpandRight: position is not right");
-            Assert.AreEqual(audioClickForTest.Lenght,
-                initialLength + 1,
-                "Failed ExpandRight: length is not right");
         }
     }
 }
