159708eddb72f3e866db1f295e42cc32bb0018b3
DmitriiKh <31587652+DmitriiKh@users.noreply.github.com>
2018-04-02 19:00:34 -0300

    Refactored BurgPredictionCalculatorTests

--- GPU_Declicker_Tests/AudioProcessingTests.cs
+++ GPU_Declicker_Tests/AudioProcessingTests.cs
@@ -8,35 +8,6 @@ namespace GPU_Declicker_Tests
     public class AudioProcessingTests
     {
         [TestMethod]
-        public void BurgPredictionCalculator_Calculate()
-        {
-            const int coef_number = 4;
-            const int history_length = 512;
-
-            float[] input_audio = new float[history_length + 1];
-            float[] forwardPredictions = new float[history_length + 1];
-            float[] backwardPredictions = new float[history_length + 1];
-
-            for (int i = 0; i < input_audio.Length; i++)
-            {
-                input_audio[i] = (float) Math.Sin(2 * Math.PI * i / (history_length / 5.2));
-            }
-
-            BurgPredictionCalculator.Calculate(
-                input_audio,
-                forwardPredictions,
-                backwardPredictions,
-                history_length,
-                coef_number,
-                history_length);
-                
-            Assert.AreEqual(
-                forwardPredictions[forwardPredictions.Length - 1],
-                input_audio[input_audio.Length - 1],
-                0.0001);
-        }
-
-        [TestMethod]
         public void AudioProcessingCalc_burg_pred()
         {
             const int history_length = 512;
---
+++ GPU_Declicker_Tests/BurgPredictionCalculatorTests.cs
@@ -0,0 +1,49 @@
+﻿using GPU_Declicker_UWP_0._01;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace GPU_Declicker_Tests
+{
+    [TestClass]
+    public class BurgPredictionCalculatorTests
+    {
+        [TestMethod]
+        public void Calculate()
+        {
+            const int coefNumber = 4;
+            const int historyLength = 512;
+            const int numberOfSamplesToCheck = 10;
+
+            float[] input_audio =
+                new float[historyLength + numberOfSamplesToCheck];
+            float[] forwardPredictions =
+                new float[historyLength + numberOfSamplesToCheck];
+            float[] backwardPredictions =
+                new float[historyLength + numberOfSamplesToCheck];
+
+            for (int i = 0; i < input_audio.Length; i++)
+            {
+                input_audio[i] = (float)Math.Sin(
+                    2 * Math.PI * i / (historyLength / 5.2));
+            }
+
+            for (int index = historyLength;
+                index < historyLength + numberOfSamplesToCheck;
+                index++)
+            {
+                BurgPredictionCalculator.Calculate(
+                    input_audio,
+                    forwardPredictions,
+                    backwardPredictions,
+                    index,
+                    coefNumber,
+                    historyLength);
+
+                Assert.AreEqual(
+                    forwardPredictions[index],
+                    input_audio[index],
+                    0.000001);
+            }
+        }
+    }
+}
