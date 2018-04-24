607f26662d9490e2025ed5f86b97bc0c630626fb
DmitriiKh <31587652+DmitriiKh@users.noreply.github.com>
2018-04-24 05:46:55 -0300

    Tests for AudioData.OnClickChanged

---
+++ GPU_Declicker_Tests/AudioDataTests.cs
@@ -0,0 +1,68 @@
+﻿using GPU_Declicker_UWP_0._01;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace GPU_Declicker_Tests
+{
+    [TestClass]
+    public class AudioSataTests
+    {
+        AudioData _audioData;
+        AudioClick _audioClick;
+
+        [TestInitialize]
+        public void AudioDataBeforeRunningTest()
+        {
+            var inputAudio = new float[4096];
+            for (int index = 0; index < inputAudio.Length; index++)
+                inputAudio[index] = 0.5f;
+            for (int index = 1051; index < 1059; index++)
+                inputAudio[index] = -0.5f;
+
+
+            _audioData = new AudioDataMono(inputAudio);
+            for (int index = 0; index < inputAudio.Length; index++)
+                _audioData.SetOutputSample(
+                    index, 
+                    _audioData.GetInputSample(index));
+
+            _audioClick = new AudioClick(
+                1051, 
+                10, 
+                111, 
+                new AudioDataMono(inputAudio), 
+                ChannelType.Left);
+        }
+
+        [TestMethod]
+        public void OnClickChanged_Shrinked_RestoreSampleAndRepair()
+        {
+            _audioData.SetOutputSample(_audioClick.Position - 1, 0);
+            _audioData.SetOutputSample(_audioClick.Position + _audioClick.Lenght, 0);
+
+            _audioData.OnClickChanged(
+                _audioClick, 
+                new ClickEventArgs { Shrinked = true });
+
+            for (int index = 0; index < _audioData.LengthSamples(); index++)
+                Assert.AreEqual(
+                    0.5f, 
+                    _audioData.GetOutputSample(index), 
+                    "error at index " + index.ToString());
+        }
+
+        [TestMethod]
+        public void OnClickChanged_NotShrinked_Repair()
+        {
+            _audioData.OnClickChanged(
+                _audioClick,
+                new ClickEventArgs { Shrinked = true });
+
+            for (int index = 0; index < _audioData.LengthSamples(); index++)
+                Assert.AreEqual(
+                    0.5f,
+                    _audioData.GetOutputSample(index),
+                    "error at index " + index.ToString());
+        }
+    }
+}
