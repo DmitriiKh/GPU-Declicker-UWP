8691b221cece9853e3c3b14b186f7c334e94a6a5
DmitriiKh <31587652+DmitriiKh@users.noreply.github.com>
2018-05-10 16:47:11 -0300

    Created test class for AudioViewer

---
+++ GPU_Declicker_Tests/AudioViewerTests.cs
@@ -0,0 +1,11 @@
+﻿using GPU_Declicker_UWP_0._01;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GPU_Declicker_Tests
+{
+    [TestClass]
+    public class AudioViewerTests
+    {
+
+    }
+}
