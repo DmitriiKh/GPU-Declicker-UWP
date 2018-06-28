ec86665652d722137e565bd7a84d2f0256992aef
DmitriiKh <31587652+DmitriiKh@users.noreply.github.com>
2018-06-28 18:46:23 -0300

    Readability improvment

--- GPU Declicker UWP 0.01/BurgPredictionCalculator.cs
+++ GPU Declicker UWP 0.01/BurgPredictionCalculator.cs
@@ -1,4 +1,6 @@
-﻿namespace GPU_Declicker_UWP_0._01
+﻿using System;
+
+namespace GPU_Declicker_UWP_0._01
 {
     public static class BurgPredictionCalculator
     {
@@ -15,72 +17,71 @@
             int coefficientsNumber,
             int historyLengthSamples)
         {
-            double[] b = new double[historyLengthSamples];
-            double[] f = new double[historyLengthSamples];
-            double[] a = new double[(coefficientsNumber + 1)];
-            double ACCUM;
-            double D = 0.0, mu = 0.0;
+            var b = new double[historyLengthSamples];
+            var f = new double[historyLengthSamples];
+            var a = new double[(coefficientsNumber + 1)];
 
-            for (int I = 0; I < historyLengthSamples; I++)
+            for (var I = 0; I < historyLengthSamples; I++)
             {
                 b[I] = inputaudio[I + position - historyLengthSamples];
                 f[I] = b[I];
             }
 
-            int N = historyLengthSamples - 1;
+            var indexN = historyLengthSamples - 1;
 
-            for (int I = 1; I <= coefficientsNumber; I++)
+            for (var I = 1; I <= coefficientsNumber; I++)
                 a[I] = 0.0;
             a[0] = 1.0;
 
-            D = 0.0;
-            for (int I = 0; I < historyLengthSamples; I++)
-                D += 2.0 * f[I] * f[I];
-            D -= f[0] * f[0] + b[N] * b[N];
+            var d = 0.0;
+            for (var I = 0; I < historyLengthSamples; I++)
+                d += 2.0 * f[I] * f[I];
+            d -= f[0] * f[0] + b[indexN] * b[indexN];
 
-            for (int k = 0; k < coefficientsNumber; k++)
+            for (var k = 0; k < coefficientsNumber; k++)
             {
-                mu = 0.0;
-                for (int n = 0; n <= N - k - 1; n++)
+                var mu = 0.0;
+
+                for (var n = 0; n <= indexN - k - 1; n++)
                     mu += f[n + k + 1] * b[n];
 
-                if (mu != 0)
-                    mu *= -2.0 / D;
+                if (Math.Abs(mu) > 0)
+                    mu *= -2.0 / d;
 
-                for (int n = 0; n <= (k + 1) / 2; n++)
+                for (var n = 0; n <= (k + 1) / 2; n++)
                 {
-                    double t1 = a[n] + mu * a[k + 1 - n];
-                    double t2 = a[k + 1 - n] + mu * a[n];
+                    var t1 = a[n] + mu * a[k + 1 - n];
+                    var t2 = a[k + 1 - n] + mu * a[n];
                     a[n] = t1;
                     a[k + 1 - n] = t2;
                 }
 
-                for (int n = 0; n <= N - k - 1; n++)
+                for (var n = 0; n <= indexN - k - 1; n++)
                 {
-                    double t1 = f[n + k + 1] + mu * b[n];
-                    double t2 = b[n] + mu * f[n + k + 1];
+                    var t1 = f[n + k + 1] + mu * b[n];
+                    var t2 = b[n] + mu * f[n + k + 1];
                     f[n + k + 1] = t1;
                     b[n] = t2;
                 }
 
-                D = (1.0 - mu * mu) * D -
+                d = (1.0 - mu * mu) * d -
                     f[k + 1] * f[k + 1]
-                    - b[N - k - 1] * b[N - k - 1];
+                    - b[indexN - k - 1] * b[indexN - k - 1];
             }
 
-            ACCUM = 0.0;
-            for (int I = 1; I <= coefficientsNumber; I++)
-                ACCUM += inputaudio[position - I] * (-1) * a[I];
+            var accum = 0.0;
+            for (var I = 1; I <= coefficientsNumber; I++)
+                accum += inputaudio[position - I] * (-1) * a[I];
 
-            forwardPredictions[position] = (float)ACCUM;
+            forwardPredictions[position] = (float)accum;
 
-            ACCUM = 0.0;
-            for (int I = 1; I <= coefficientsNumber; I++)
-                ACCUM += inputaudio[position - historyLengthSamples + I] *
+            accum = 0.0;
+            for (var I = 1; I <= coefficientsNumber; I++)
+                accum += inputaudio[position - historyLengthSamples + I] *
                     (-1) * a[I];
 
             backwardPredictions[position - historyLengthSamples] =
-                (float)ACCUM;
+                (float)accum;
         }
     }
 }
